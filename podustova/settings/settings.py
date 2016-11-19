63a7451b20b6f824b7f522334471d59a033eef8b
Anton Gramenko <tony1986@yandex.ru>
2016-11-19 14:44:41 +0200

    Initial commit STATICROOT changes

--- podustova/settings/settings.py
+++ podustova/settings/settings.py
@@ -150,13 +150,13 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
